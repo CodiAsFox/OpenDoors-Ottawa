b38c97983117202f6684032fa5dda8df8c49e5e6
Taylor Ramirez <rami0101@algonquinlive.com>
2023-12-11 23:28:39 -0500

    Final project progress

---
+++ DoorsOpenOttawa/Model/LanguageManager.swift
@@ -0,0 +1,8 @@
+//
+//  LanguageManager.swift
+//  DoorsOpenOttawa
+//
+//  Created by Tay on 2023-12-11.
+//
+
+import Foundation
---
+++ DoorsOpenOttawa/Views/MoreView.swift
@@ -0,0 +1,18 @@
+//
+//  MoreView.swift
+//  DoorsOpenOttawa
+//
+//  Created by Tay on 2023-12-11.
+//
+
+import SwiftUI
+
+struct MoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoreView()
+}
